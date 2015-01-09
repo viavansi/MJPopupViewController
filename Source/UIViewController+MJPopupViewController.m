bed63eb78aad8d764d4bf82a7653643191fda69c
Juan Ant. Garrido <jgarrido@avansi.es>
2015-01-09 08:28:28 +0100

    Unshaded alert

--- Source/UIViewController+MJPopupViewController.m
+++ Source/UIViewController+MJPopupViewController.m
@@ -110,9 +110,9 @@ static void * const keypath = (void*)&keypath;
     // customize popupView
     popupView.layer.shadowPath = [UIBezierPath bezierPathWithRect:popupView.bounds].CGPath;
     popupView.layer.masksToBounds = NO;
-    popupView.layer.shadowOffset = CGSizeMake(5, 5);
-    popupView.layer.shadowRadius = 5;
-    popupView.layer.shadowOpacity = 0.5;
+//    popupView.layer.shadowOffset = CGSizeMake(5, 5);
+//    popupView.layer.shadowRadius = 5;
+//    popupView.layer.shadowOpacity = 0.5;
     popupView.layer.shouldRasterize = YES;
     popupView.layer.rasterizationScale = [[UIScreen mainScreen] scale];
     
